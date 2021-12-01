4f6040c37a689a5b23c0565a0027f2d3cb2b8df6
Stalker165 <minaev.artem@phystech.edu>
2021-12-01 20:33:13 +0300

    turret.py v 0.5: HUGE refactoring: enemies spawn implementation now by functions 3 types of tankettes with different size, speed, rate of fire, damage, frequency of spawn!

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ BULLET_SPEED = 64
 mouse_x = 0
 mouse_y = 0
 GROUND = 60
-TANK_VELOCITY = 1
+TANKETTE_VELOCITIES = [2, 1.5, 1]
 DRONE_VELOCITY = 3
 FPS = 60
 BULLET_PENETRA = 2  # armor penetration
@@ -87,32 +87,10 @@ class MyGame(arcade.Window):
         if not self.game_over:
             self.frame_count += 1
 
-            if self.frame_count % (FPS * 2) == 0:
-                # tankette spawn: спавн танкетки
-                direct = random.choice([1, -1])
-                if direct == -1:
-                    tankette = arcade.Sprite("pictures/tankette.png", flipped_horizontally=True)
-                else:
-                    tankette = arcade.Sprite("pictures/tankette.png")
-                tankette.direct = direct
-                tankette.change_x = direct * TANK_VELOCITY
-                tankette.center_x = SCREEN_WIDTH/2 - direct * (SCREEN_WIDTH/2 + tankette.width)
-                tankette.top = GROUND + tankette.height/2
-                tankette.hp = 5
-                tankette.recharge_time = 0
-                self.ground_enemies.append(tankette)
-
-            if self.frame_count % (FPS * 2) == 0:
-                # drone spawn: спавн дрона
-                direct = random.choice([1, -1])
-                drone = arcade.Sprite("pictures/drone.png")
-                drone.center_x = SCREEN_WIDTH/2 - direct * (SCREEN_WIDTH/2 + drone.width)
-                drone.top = random.randint(SCREEN_HEIGHT//2, SCREEN_HEIGHT)
-                angle = math.atan2(drone.center_y - self.objects[0].center_y, drone.center_x - self.objects[0].center_x)
-                drone.change_x = - DRONE_VELOCITY * math.cos(angle)
-                drone.change_y = - DRONE_VELOCITY * math.sin(angle)
-                drone.hp = 1
-                self.enemy_kamikaze.append(drone)
+            self.default_tankette_spawn(frequency=3, size=0)
+            self.default_tankette_spawn(frequency=6, size=1)
+            self.default_tankette_spawn(frequency=11, size=2)
+            self.default_drone_spawn(frequency=5)
 
             for gun in self.guns:
                 angle = math.atan2(mouse_y - gun.center_y, mouse_x - gun.center_x)
@@ -140,7 +118,12 @@ class MyGame(arcade.Window):
                 if self.enemy_bullet_list is not None:
                     hit_list = arcade.check_for_collision_with_list(object, self.enemy_bullet_list)
                     for bullet in hit_list:
-                        object.hp -= 2
+                        if bullet.size == 0:
+                            object.hp -= 1
+                        if bullet.size == 1:
+                            object.hp -= 2
+                        if bullet.size == 2:
+                            object.hp -= 4
                         bullet.remove_from_sprite_lists()
                     if object.hp <= 0:
                         object.texture = arcade.load_texture("pictures/pillbox_destructed.png")
@@ -149,9 +132,17 @@ class MyGame(arcade.Window):
             for enemy in self.ground_enemies:
                 if enemy.hp <= 0:
                     enemy.remove_from_sprite_lists()
-                elif abs(enemy.center_x - SCREEN_WIDTH/2) < SCREEN_WIDTH/6:
-                    self.fire(enemy)
-                    enemy.change_x = 0
+                else:
+                    distance = abs(enemy.center_x - SCREEN_WIDTH / 2)
+                    if enemy.size == 0 and distance < SCREEN_WIDTH/6:
+                        self.fire(enemy)
+                        enemy.change_x = 0
+                    if enemy.size == 1 and distance < SCREEN_WIDTH/5:
+                        self.fire(enemy)
+                        enemy.change_x = 0
+                    if enemy.size == 2 and distance < SCREEN_WIDTH/4:
+                        self.fire(enemy)
+                        enemy.change_x = 0
 
             for enemy in self.enemy_kamikaze:
                 if enemy.hp <= 0:
@@ -206,17 +197,29 @@ class MyGame(arcade.Window):
     def fire(self, enemy):
         """ Makes enemy firing: creates bullets if enemy is aimed and keep aiming if not """
         if enemy.recharge_time == 0:
-            bullet = arcade.Sprite("pictures/bullet.png", 1)
+            bullet = arcade.Sprite("pictures/" + str(enemy.size) + "_bullet.png", 1)
+            bullet.size = enemy.size
             bullet.center_x = enemy.center_x
             bullet.center_y = enemy.center_y
             bullet.change_x = BULLET_SPEED * enemy.direct
-            enemy.recharge_time = 75
+            if enemy.size == 0:
+                enemy.recharge_time = 30
+            elif enemy.size == 1:
+                enemy.recharge_time = 60
+            elif enemy.size == 2:
+                enemy.recharge_time = 75
             self.enemy_bullet_list.append(bullet)
         else:
             enemy.recharge_time -= 1
 
     def create_boom(self, x, y, Vx, Vy):
-        """ Creates an explosion. boom.count helps for switching frames """
+        """
+        Creates an explosion. boom.count helps for switching frames
+        :param x: x coordinate of center
+        :param y: y coordinate of center
+        :param Vx: velocity projection on the x-axis
+        :param Vy: velocity projection on the y-axis
+        """
         boom = arcade.Sprite("pictures/boom1.png")
         boom.center_x = x + Vx
         boom.center_y = y + Vy
@@ -224,6 +227,49 @@ class MyGame(arcade.Window):
         boom.change_y = Vy/3 - 1  # and slowly falls down
         boom.count = FPB
         self.booms.append(boom)
+
+    def default_tankette_spawn(self, frequency, size, recharge_time=0):
+        """
+        Creates a tankette
+        :param frequency: frequency of spawn
+        :param size: size using for creating bullets
+        :param recharge_time: initial recharge time
+        """
+        if self.frame_count % (FPS * frequency) == 0:
+            direct = random.choice([1, -1])
+            if direct == -1:
+                tankette = arcade.Sprite("pictures/" + str(size) + "_tankette.png", flipped_horizontally=True)
+            else:
+                tankette = arcade.Sprite("pictures/" + str(size) + "_tankette.png")
+            tankette.size = size
+            tankette.direct = direct
+            tankette.change_x = direct * TANKETTE_VELOCITIES[size]
+            tankette.center_x = SCREEN_WIDTH/2 - direct * (SCREEN_WIDTH/2 + tankette.width)
+            tankette.bottom = GROUND/2 - 4
+            if size == 0:
+                tankette.hp = 3
+            elif size == 1:
+                tankette.hp = 6
+            elif size == 2:
+                tankette.hp = 10
+            tankette.recharge_time = recharge_time
+            self.ground_enemies.append(tankette)
+
+    def default_drone_spawn(self, frequency):
+        """
+        Creates a drone
+        :param frequency: frequency of spawn
+        """
+        if self.frame_count % (FPS * frequency) == 0:
+            direct = random.choice([1, -1])
+            drone = arcade.Sprite("pictures/drone.png")
+            drone.center_x = SCREEN_WIDTH / 2 - direct * (SCREEN_WIDTH / 2 + drone.width)
+            drone.top = random.randint(SCREEN_HEIGHT // 2, SCREEN_HEIGHT)
+            angle = math.atan2(drone.center_y - self.objects[0].center_y, drone.center_x - self.objects[0].center_x)
+            drone.change_x = - DRONE_VELOCITY * math.cos(angle)
+            drone.change_y = - DRONE_VELOCITY * math.sin(angle)
+            drone.hp = 1
+            self.enemy_kamikaze.append(drone)
 
 
 def main():
