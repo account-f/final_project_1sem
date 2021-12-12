a39cf4904a36a4b07fc682b56e774ef959bbaf65
Stalker165 <minaev.artem@phystech.edu>
2021-12-12 17:35:25 +0300

    main.py v 0.87: huge spawn update! preparing for complicating enemy spawn

--- main.py
+++ main.py
@@ -13,17 +13,25 @@ GROUND = 80  # высота земли
 
 # объекты:
 BULLET_SPEED = 48  # полная скорость пули
-TANKETTE_VELOCITIES = [2, 1.5, 1]  # полные скорости танкеток (в соответствии с размером)
-TANKETTE_HPS = [4, 8, 12]
+TANKETTE_VELOCITIES = [2, 1.5, 0, 1]  # полные скорости танкеток (в соответствии с размером)
+TANKETTE_HPS = [4, 8, 0, 12]
 DRONE_VELOCITY = 2.5  # полная скорость дрона
 COPTER_VELOCITY = 3  # полная скорости коптера
 COPTER_HPS = [3, 6]
 BULLET_PENETRA = 3  # HP пули главной пушки
 TOLERANCE = SCREEN_WIDTH / 10
 
+enemy_points = 20  # initial number of enemy points, using to spawn
+
+ENEMIES = [0] * 5
+ENEMIES[4] = ["tankette"]
+ENEMIES[2] = ["tankette"]
+ENEMIES[1] = ["tankette", "drone", "copter1", "copter2"]  # list of enemies which cost 1
+
 # время:
 FPS = 60
 FPB = 4  # frames per one texture image of explosion
+SPAWN_INTERVAL = FPS*10
 
 
 class MyGame(arcade.Window):
@@ -38,8 +46,10 @@ class MyGame(arcade.Window):
         self.game_over = False
         arcade.set_background_color((255, 255, 255))
 
-        self.frame_count = 0  # счётчик обновлений экрана
+        self.frame_count = -1  # счётчик обновлений экрана
         self.score = 0  # счёт
+        self.enemy_points = 0  # текущее значение очков врага
+        self.spawn_timer = 0  # таймер, по которому рассчитывается спавн врагов
 
         self.enemies = None  # все враги
         self.ground_enemies = None  # наземные враги
@@ -51,6 +61,7 @@ class MyGame(arcade.Window):
         self.player_guns = None  # пушки игрока
         self.guns = None  # пушки врагов
         self.booms = None  # список действующих анимаций взрывов
+        self.spawn_list = []  # list contains name of enemy, size and random time of spawn
 
         # инициализация звуков
         self.main_sound = arcade.load_sound("sounds/background.m4a", False)
@@ -142,12 +153,7 @@ class MyGame(arcade.Window):
             # обновление счетчика кадров:
             self.frame_count += 1
 
-            # регулярный спавн танкеток дронов и коптеров:
-            self.default_tankette_spawn(frequency=3, size=0)
-            self.default_tankette_spawn(frequency=6, size=1)
-            self.default_tankette_spawn(frequency=11, size=2)
-            self.default_drone_spawn(frequency=5)
-            self.default_copter_spawn(frequency=3, size=0, type=random.choice([0, 1]))
+            self.spawn()
 
             for gun in self.player_guns:
                 # обновление угла наклона пушки игрока
@@ -178,7 +184,7 @@ class MyGame(arcade.Window):
                 # обновление HP игрока и замена текстуры холма при его занулении
                 hit_list = arcade.check_for_collision_with_list(object, self.enemy_bullet_list)
                 for bullet in hit_list:
-                    object.hp -= 2**bullet.size
+                    object.hp -= bullet.size
 
                     # зануление HP игрока при его возможном достижении отрицательного значения:
                     if object.hp < 0:
@@ -198,18 +204,14 @@ class MyGame(arcade.Window):
                 if enemy.hp <= 0:
                     # удаление танкетки при отсутствии у нее HP:
                     enemy.remove_from_sprite_lists()
-                    self.score += enemy.size + 1  # начисление очков в зависимости от размера врага
+                    self.score += enemy.size  # начисление очков в зависимости от размера врага
                 else:
                     distance = abs(enemy.center_x - SCREEN_WIDTH / 2)
 
                     # остановка танкетки на определенном расстоянии от холма (в зависимости от размера):
-                    if enemy.size == 0 and distance < SCREEN_WIDTH/6:
-                        self.fire(enemy)
-                        enemy.change_x = 0
-                    elif enemy.size == 1 and distance < SCREEN_WIDTH/5:
-                        self.fire(enemy)
-                        enemy.change_x = 0
-                    elif enemy.size == 2 and distance < SCREEN_WIDTH/4:
+                    if ((enemy.size == 1 and distance < SCREEN_WIDTH/6) or
+                            (enemy.size == 2 and distance < SCREEN_WIDTH/5) or
+                            (enemy.size == 4 and distance < SCREEN_WIDTH/4)):
                         self.fire(enemy)
                         enemy.change_x = 0
                     else:
@@ -224,11 +226,11 @@ class MyGame(arcade.Window):
                 if enemy.hp <= 0:
                     enemy.remove_from_sprite_lists()
                     enemy.gun.remove_from_sprite_lists()
-                    self.score += enemy.size + 1  # начисление очков в зависимости от размера врага
+                    self.score += enemy.size  # начисление очков в зависимости от размера врага
                 else:
                     if enemy.time % 3 == 0:
                         self.next_frame(enemy, frames=6)
-                    if enemy.type == 0 and enemy.time >= FPS:
+                    if enemy.type == 2 and enemy.time >= FPS:
                         self.fire(enemy.gun)
                     if ((enemy.center_x - self.objects[0].center_x) ** 2 +
                             (enemy.center_y - self.objects[0].center_y) ** 2
@@ -244,7 +246,7 @@ class MyGame(arcade.Window):
                     # удаление дрона с созданием на его месте взрыва:
                     self.create_boom(enemy.center_x, enemy.center_y, enemy.change_x, enemy.change_y)
                     enemy.remove_from_sprite_lists()
-                    self.score += enemy.size + 1  # начисление очков в зависимости от размера врага
+                    self.score += enemy.size  # начисление очков в зависимости от размера врага
 
                     # включение звука взрыва:
                     arcade.play_sound(self.helicopter_crash, volume=0.3)
@@ -332,11 +334,11 @@ class MyGame(arcade.Window):
             bullet.angle = math.degrees(angle)
             bullet.change_x = - math.cos(angle) * BULLET_SPEED
             bullet.change_y = - math.sin(angle) * BULLET_SPEED
-            if enemy.size == 0:
+            if enemy.size == 1:
                 enemy.recharge_time = 30
-            elif enemy.size == 1:
-                enemy.recharge_time = 50
             elif enemy.size == 2:
+                enemy.recharge_time = 50
+            elif enemy.size == 4:
                 enemy.recharge_time = 75
             self.enemy_bullet_list.append(bullet)
         else:
@@ -390,90 +392,98 @@ class MyGame(arcade.Window):
         boom.count = FPB
         self.booms.append(boom)
 
-    def default_tankette_spawn(self, frequency, size, recharge_time=0):
+    def default_tankette_spawn(self, size, recharge_time=0):
         """
         Creates a tankette
-        :param frequency: frequency of spawn
         :param size: size using for creating bullets
         :param recharge_time: initial recharge time
         """
-        if self.frame_count % (FPS * frequency) == 0:
-            direct = random.choice([1, -1])
-            file = "pictures/" + str(size) + "_tankette 0 .png"
-            tankette = arcade.Sprite(file, flipped_horizontally=bool(abs(direct-1)/2))
-            tankette.file = file
-            tankette.size = size
-            tankette.direct = direct
-            tankette.time = 0  # initial existing time
-            tankette.change_x = direct * TANKETTE_VELOCITIES[size]
-            tankette.center_x = SCREEN_WIDTH/2 - direct * (SCREEN_WIDTH/2 + tankette.width)
-            tankette.bottom = GROUND/2
-            tankette.hp = TANKETTE_HPS[size]  # установление HP танкетки согласно ее размеру
-            tankette.recharge_time = recharge_time
-            self.ground_enemies.append(tankette)
-            self.enemies.append(tankette)
+        direct = random.choice([1, -1])
+        file = "pictures/" + str(size) + "_tankette 0 .png"
+        tankette = arcade.Sprite(file, flipped_horizontally=bool(abs(direct-1)/2))
+        tankette.file = file
+        tankette.size = size
+        tankette.direct = direct
+        tankette.time = 0  # initial existing time
+        tankette.change_x = direct * TANKETTE_VELOCITIES[size - 1]
+        tankette.center_x = SCREEN_WIDTH/2 - direct * (SCREEN_WIDTH/2 + tankette.width)
+        tankette.bottom = GROUND/2
+        tankette.hp = TANKETTE_HPS[size - 1]  # установление HP танкетки согласно ее размеру
+        tankette.recharge_time = recharge_time
+        self.ground_enemies.append(tankette)
+        self.enemies.append(tankette)
 
-    def default_drone_spawn(self, frequency):
+    def default_drone_spawn(self, size):
         """
         Creates a drone
-        :param frequency: frequency of spawn
+        :param size: size
         """
-        if self.frame_count % (FPS * frequency) == 0:
-            direct = random.choice([1, -1])
-            drone = arcade.Sprite("pictures/drone.png")
-            drone.size = 0
-            drone.center_x = SCREEN_WIDTH/2 - direct * (SCREEN_WIDTH/2 + drone.width)
-            drone.top = random.randint(SCREEN_HEIGHT//2, SCREEN_HEIGHT)
-            angle = math.atan2(drone.center_y - self.objects[0].center_y, drone.center_x - self.objects[0].center_x)
-            drone.change_x = - DRONE_VELOCITY * math.cos(angle)
-            drone.change_y = - DRONE_VELOCITY * math.sin(angle)
-            drone.hp = 1
+        direct = random.choice([1, -1])
+        drone = arcade.Sprite("pictures/drone.png")
+        drone.size = size
+        drone.center_x = SCREEN_WIDTH/2 - direct * (SCREEN_WIDTH/2 + drone.width)
+        drone.top = random.randint(SCREEN_HEIGHT//2, SCREEN_HEIGHT)
+        angle = math.atan2(drone.center_y - self.objects[0].center_y, drone.center_x - self.objects[0].center_x)
+        drone.change_x = - DRONE_VELOCITY * math.cos(angle)
+        drone.change_y = - DRONE_VELOCITY * math.sin(angle)
+        drone.hp = 1
 
-            # добавление дрона к соответствующим спискам врагов:
-            self.enemy_kamikaze.append(drone)
-            self.enemies.append(drone)
+        # добавление дрона к соответствующим спискам врагов:
+        self.enemy_kamikaze.append(drone)
+        self.enemies.append(drone)
 
-    def default_copter_spawn(self, frequency, size, type):
+    def default_copter1_spawn(self, size):
         """
         Creates a (heli)copter
-        :param frequency: frequency of spawn
         :param size: size
         helicopter, helicopter
         """
-        if self.frame_count % (FPS * frequency) == 0:
-            direct = random.choice([1, -1])
-            file = "pictures/" + str(size) + "_copter 0 .png"
-            copter = arcade.Sprite(file)
-            copter.type = type
-            copter.center_x = SCREEN_WIDTH/2 - direct * (SCREEN_WIDTH/2 + copter.width)
-            copter.top = random.randint(SCREEN_HEIGHT//2, SCREEN_HEIGHT)
-            copter.direct = direct
-            copter.file = file
-            copter.size = size
-            copter.hp = COPTER_HPS[size]
-            copter.time = 0  # initial existing time
+        direct = random.choice([1, -1])
+        file = "pictures/" + str(size) + "_copter 0 .png"
+        copter = arcade.Sprite(file)
+        copter.type = 1
+        copter.center_x = SCREEN_WIDTH/2 - direct * (SCREEN_WIDTH/2 + copter.width)
+        copter.top = random.randint(SCREEN_HEIGHT//2, SCREEN_HEIGHT)
+        copter.direct = direct
+        copter.file = file
+        copter.size = size
+        copter.hp = COPTER_HPS[size - 1]
+        copter.time = 0  # initial existing time
 
-            angle = math.atan2(self.objects[0].center_y - copter.center_y,
-                               self.objects[0].center_x - copter.center_x)
+        angle = math.atan2(self.objects[0].center_y - copter.center_y,
+                           self.objects[0].center_x - copter.center_x)
 
-            if type == 0:
-                copter.change_x = COPTER_VELOCITY * direct
-            elif type == 1:
-                copter.change_x = COPTER_VELOCITY * math.cos(angle)
-                copter.change_y = COPTER_VELOCITY * math.sin(angle)
+        copter.change_x = COPTER_VELOCITY * math.cos(angle)
+        copter.change_y = COPTER_VELOCITY * math.sin(angle)
 
-            copter.gun = arcade.Sprite("pictures/gun1.png")
-            copter.gun.size = 0
-            copter.gun.angle = math.degrees(angle) - 90
-            copter.gun.center_x = copter.center_x
-            copter.gun.top = copter.center_y + 8
-            copter.gun.recharge_time = 0
-            copter.gun.change_x = copter.change_x
-            copter.gun.change_y = copter.change_y
+        copter.gun = arcade.Sprite("pictures/gun1.png")
+        copter.gun.size = 1
+        copter.gun.angle = math.degrees(angle) - 90
+        copter.gun.center_x = copter.center_x
+        copter.gun.top = copter.center_y + 8
+        copter.gun.recharge_time = 0
+        copter.gun.change_x = copter.change_x
+        copter.gun.change_y = copter.change_y
 
-            self.air_enemies.append(copter)
-            self.enemies.append(copter)
-            self.guns.append(copter.gun)
+        self.air_enemies.append(copter)
+        self.enemies.append(copter)
+        self.guns.append(copter.gun)
+
+    def default_copter2_spawn(self, size):
+        """
+        Creates a (heli)copter using same function for other type.
+        Changes variables using sprite lists.
+        :param size: size
+        helicopter, helicopter
+        """
+        self.default_copter1_spawn(size)
+
+        self.air_enemies[-1].change_x = COPTER_VELOCITY * self.air_enemies[-1].direct
+        self.air_enemies[-1].change_y = 0
+
+        self.air_enemies[-1].gun.change_x = self.air_enemies[-1].change_x
+        self.air_enemies[-1].gun.change_y = self.air_enemies[-1].change_y
+        self.air_enemies[-1].type = 2
 
     def next_frame(self, object, frames=2):
         """
@@ -488,6 +498,43 @@ class MyGame(arcade.Window):
             number = -1
         object.file = object.file.split(" ")[0] + " " + str((number + 1) % frames) + " " + object.file.split(" ")[2]
         object.texture = arcade.load_texture(object.file, flipped_horizontally=bool(abs(object.direct-1)/2))
+
+    def spawn(self):
+        """
+        Increases the level of difficulty. Spawns enemies depending on the enemy_points
+        Big tankette "costs" 4 points, so game can generate it from 0 to 5, if enemy_points=20.
+        Next it generates enemies which costs 2 points using remained points.
+        If after all spawn there are points left, it goes to the next cycle.
+        Creates list of enemies which spawn at this cycle.
+        """
+        if self.spawn_timer == 0:
+            self.spawn_list = []
+
+            numbers = [0] * 5
+            self.enemy_points += enemy_points
+            numbers[2] = random.randint(0, self.enemy_points // 2)
+            self.enemy_points -= numbers[2] * 2
+            numbers[4] = random.randint(0, self.enemy_points // 4)
+            self.enemy_points -= numbers[4] * 4
+            numbers[1] = random.randint(0, self.enemy_points // 1)
+            self.enemy_points -= numbers[1]
+
+            for i in (1, 2, 4):
+                # фиксируем размер
+                for elem in ENEMIES[i]:
+                    # фиксируем элемент
+                    current_number = random.randint(0, numbers[i])
+                    for _ in range(current_number):
+                        self.spawn_list.append([elem, i, random.randint(0, SPAWN_INTERVAL)])
+                    numbers[i] -= current_number
+                self.spawn_list.append([ENEMIES[i][len(ENEMIES[i])-1], i, random.randint(0, SPAWN_INTERVAL)])
+
+            self.spawn_timer = SPAWN_INTERVAL
+        else:
+            self.spawn_timer -= 1
+            for i in range(len(self.spawn_list)):
+                if self.spawn_timer == self.spawn_list[i][2]:
+                    eval("self.default_" + str(self.spawn_list[i][0]) + "_spawn(" + str(self.spawn_list[i][1]) + ")")
 
 
 def main():
